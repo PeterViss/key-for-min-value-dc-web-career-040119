c2dd8198add54d4d660ec13b36f8d6cea04c8de3
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-06 03:49:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,16 +1,15 @@
 def key_for_min_value(name_hash)
-
-   i = 10000
-  min_key = {}
-  if name_hash.length > 0
-    name_hash.each do |key, value|
-      if value < i
-        min_key = key
-        i = value
-      end
-    end
-    min_key
-  else
-    nil
-  end
-end 	
+small_hash = {}
+name_hash.each do |name, number|
+  if number < 10000
+    nu_number = number
+  if number < nu_number
+    two_number = number
+  if number < two_number
+    small_hash << number
+end
+end
+end
+end
+small_hash
+end
