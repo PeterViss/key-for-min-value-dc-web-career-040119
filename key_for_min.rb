cfa614e20b57576e1e2415bf26ca3132c7f5055f
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-06 04:19:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,13 +1,11 @@
-def key_for_min_value(name_hash)
-small_hash = {}
-name_hash.each do |name, number|
-  if number < 10000
-    nu_number = number
-  elsif  number < nu_number
-    two_number = number
-  elsif number < two_number
-    cool_number = number
-end
-end
-nil
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = Float::INFINITY
+  hash.each do |k, v|
+    if v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
 end
